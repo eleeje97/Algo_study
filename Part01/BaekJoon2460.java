8ece77154d53992d5bcdd3cac50516ca161b5f3a
daeunLee <eleeje97@gmail.com>
2021-11-27 00:01:03 +0900

    Add Part01/BaekJoon2460.java

--- Part01/BaekJoon2460.java
+++ Part01/BaekJoon2460.java
@@ -1,4 +1,30 @@
 package Part01;
 
+import javax.swing.plaf.IconUIResource;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BaekJoon2460 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        ArrayList<Integer> in = new ArrayList<>();
+        ArrayList<Integer> out = new ArrayList<>();
+        int current = 0;
+        int max = 0;
+
+
+        for (int i = 0; i < 10; i++) {
+            out.add(scn.nextInt());
+            in.add(scn.nextInt());
+            current -= out.get(i);
+            current += in.get(i);
+
+            max = (current > max) ? current : max;
+            System.out.println(max);
+        }
+
+
+        System.out.println(max);
+
+    }
 }
